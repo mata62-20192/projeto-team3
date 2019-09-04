219245b9f568a8e5edbfda7d914c9e55061b4c6f
Daiane <adm.daianesilva@gmail.com>
2019-09-03 22:39:11 -0300

    curso changes

--- Siac/src/main/java/br/ufba/team3/siac/model/Curso.java
+++ Siac/src/main/java/br/ufba/team3/siac/model/Curso.java
@@ -1,5 +1,6 @@
 package br.ufba.team3.siac.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
@@ -12,4 +13,83 @@ public class Curso {
     private String baseLegal;
     private String descricaoProfissional;
     private List<CursoDisciplina> disciplinas;
+
+    public Curso(String nome, Turno turno, double duracaoMinima, double duracaoMaxima, String periodo, String baseLegal, String descricaoProfissional) {
+        this.nome = nome;
+        this.turno = turno;
+        this.duracaoMinima = duracaoMinima;
+        this.duracaoMaxima = duracaoMaxima;
+        this.periodo = periodo;
+        this.baseLegal = baseLegal;
+        this.descricaoProfissional = descricaoProfissional;
+        this.disciplinas = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public double getDuracaoMinima() {
+        return duracaoMinima;
+    }
+
+    public void setDuracaoMinima(double duracaoMinima) {
+        this.duracaoMinima = duracaoMinima;
+    }
+
+    public double getDuracaoMaxima() {
+        return duracaoMaxima;
+    }
+
+    public void setDuracaoMaxima(double duracaoMaxima) {
+        this.duracaoMaxima = duracaoMaxima;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public String getBaseLegal() {
+        return baseLegal;
+    }
+
+    public void setBaseLegal(String baseLegal) {
+        this.baseLegal = baseLegal;
+    }
+
+    public String getDescricaoProfissional() {
+        return descricaoProfissional;
+    }
+
+    public void setDescricaoProfissional(String descricaoProfissional) {
+        this.descricaoProfissional = descricaoProfissional;
+    }
+
+    public List<CursoDisciplina> getDisciplinas() {
+        return disciplinas;
+    }
 }
--- Siac/src/main/java/br/ufba/team3/siac/model/CursoDisciplina.java
+++ Siac/src/main/java/br/ufba/team3/siac/model/CursoDisciplina.java
@@ -9,5 +9,50 @@ public class CursoDisciplina {
     private String nome;
     private Disciplina disciplina;
     private List<Disciplina> preRequisitos;
-    private boolean isObrigatoria;
+    private boolean isObrigatoria = false;
+
+    public CursoDisciplina(String natureza, String nome, Disciplina disciplina) {
+        this.natureza = natureza;
+        this.nome = nome;
+        this.disciplina = disciplina;
+    }
+
+    public CursoDisciplina(int semestre, String natureza, String nome) {
+        this.semestre = semestre;
+        this.natureza = natureza;
+        this.nome = nome;
+        this.isObrigatoria = true;
+    }
+
+    public void setPreRequisitos(Disciplina disciplina){
+        this.preRequisitos.add(disciplina);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public String getNatureza() {
+        return natureza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public List<Disciplina> getPreRequisitos() {
+        return preRequisitos;
+    }
+
+    public boolean isObrigatoria() {
+        return isObrigatoria;
+    }
 }
