1ef92d04b56eb1ad6337fa9f5b648ec0482f6f1e
Daiane <adm.daianesilva@gmail.com>
2019-09-03 22:14:40 -0300

    change univerisity

--- Siac/src/main/java/br/ufba/team3/siac/model/Universidade.java
+++ Siac/src/main/java/br/ufba/team3/siac/model/Universidade.java
@@ -8,4 +8,29 @@ public class Universidade {
     private List<Curso> cursos;
     private List<Disciplina> disciplinas;
 
+    public Universidade(String nome, String sigla) {
+        this.nome = nome;
+        this.sigla = sigla;
+    }
+
+    public Universidade() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+
 }
