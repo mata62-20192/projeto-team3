b1a60af85e1faa5d55c470123789012d3591ddf8
Daiane <adm.daianesilva@gmail.com>
2019-10-14 18:19:56 -0300

    remove springboot imports

--- Siac/src/main/java/br/ufba/team3/siac/Main.java
+++ Siac/src/main/java/br/ufba/team3/siac/Main.java
@@ -1,13 +1,7 @@
 package br.ufba.team3.siac;
 
 import br.ufba.team3.siac.model.*;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
