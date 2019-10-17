b166b4c40de6937470d3d2533a69144cc2501b33
Daiane <adm.daianesilva@gmail.com>
2019-10-17 20:01:55 -0300

    Fix some mistakes made in the model, and create the viewS

--- Siac/src/main/java/br/ufba/team3/siac/Main.java
+++ Siac/src/main/java/br/ufba/team3/siac/Main.java
@@ -1,6 +1,7 @@
 package br.ufba.team3.siac;
 
 
+import br.ufba.team3.siac.model.Curso;
 import br.ufba.team3.siac.model.Universidade;
 import br.ufba.team3.siac.database.UniversidadeService;
 import javafx.application.Application;
@@ -10,27 +11,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main extends Application {
 
+    static UniversidadeService universidadeService = new UniversidadeService();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(Paths.get("./src/main/resources/view/home.fxml").toUri().toURL());
+        FXMLLoader loader = new FXMLLoader(Paths.get("./src/main/resources/view/main.fxml").toUri().toURL());
         Parent root = loader.load();
         primaryStage.setTitle("New Siac");
         primaryStage.setMaximized(true);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(600);
         primaryStage.setScene(new Scene(root, 500, 600));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         try{
-            UniversidadeService universidadeService = new UniversidadeService();
             universidadeService.Leitura();
             System.out.println(universidadeService.getAllCursos());
         } catch (Exception e) {
             e.printStackTrace();
         }
         launch(args);
+    }
+
+    public static List<Curso> getAllCursos(){
+        return universidadeService.getAllCursos();
     }
 }
--- Siac/src/main/java/br/ufba/team3/siac/controller/HomeController.java
+++ Siac/src/main/java/br/ufba/team3/siac/controller/HomeController.java
@@ -7,11 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
@@ -24,22 +26,56 @@ public class HomeController implements Initializable {
     @FXML
     private Button impressaoCurriculo;
 
+    @FXML
+    private Pane secPane;
+
+    @FXML
+    private Button home;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.criarNovaCena("./src/main/resources/view/home.fxml", this.home);
+    }
+
+    public void adicionarAluno(ActionEvent event) {
+        this.criarNovaCena("./src/main/resources/view/addAluno.fxml", this.addAluno);
+    }
+
+    public void impressaoCurriculo(ActionEvent event) {
+        this.criarNovaCena("./src/main/resources/view/impressaoCurriculo.fxml", this.impressaoCurriculo);
+    }
+
+    public void loadFxml (ActionEvent event) throws IOException {
 
     }
 
-    public void adcionarAluno(ActionEvent event) {
+    public void selectButton(Button button){
+        this.addAluno.getStyleClass().remove("selected");
+        this.historicoAluno.getStyleClass().remove("selected");
+        this.impressaoCurriculo.getStyleClass().remove("selected");
+        this.home.getStyleClass().remove("selected");
+        button.getStyleClass().add("selected");
+    }
+
+    public void historicoAluno(ActionEvent actionEvent) {
+        this.criarNovaCena("./src/main/resources/view/historicoAluno.fxml", this.historicoAluno);
+    }
+
+    public void criarNovaCena(String caminho, Button button){
         try {
-            Node node = (Node) event.getSource();
-            Stage stage = (Stage) node.getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(Paths.get("./src/main/resources/view/addAluno.fxml").toUri().toURL());
-            Parent root = loader.load();
-//            AddAlunoController addAlunoController= loader.getController();
-//        val resumeController: ResumeController = loader.getController()
+            FXMLLoader loader = new FXMLLoader(Paths.get(caminho).toUri().toURL());
+            Pane newPane = loader.load();
+            List<Node> parentChildren = ((Pane)secPane.getParent()).getChildren();
+            parentChildren.set(parentChildren.indexOf(secPane), newPane);
+            secPane = newPane;
+            this.selectButton(button);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void home(ActionEvent actionEvent) {
+        this.criarNovaCena("./src/main/resources/view/home.fxml", this.home);
     }
 }
